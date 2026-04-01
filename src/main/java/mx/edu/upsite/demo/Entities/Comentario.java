2a4a2c43adba3bd419495ee2d9cef09ab9577f69
enriquecamargo <2024030456@upsin.edu.mx>
2026-04-01 01:09:18 -0700

    feat: Mapeode entidad Comentario

--- src/main/java/mx/edu/upsite/demo/Entities/Comentario.java
+++ src/main/java/mx/edu/upsite/demo/Entities/Comentario.java
@@ -1,4 +1,49 @@
 package mx.edu.upsite.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "comentarios")
 public class Comentario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_comentario")
+    private Integer id;
+
+    @Column(name = "texto", nullable = false, columnDefinition = "TEXT")
+    private String texto;
+
+    @CreationTimestamp
+    @Column(name = "fecha_comentario", updatable = false)
+    private OffsetDateTime fechaComentario;
+
+    @Column(name = "status")
+    private Integer status = 1;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_publicacion", nullable = false)
+    private Publicacion publicacion;
+
+    // RECURSIVIDAD: Relación con el comentario "padre" (si es una respuesta)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_padre")
+    private Comentario padre;
 }

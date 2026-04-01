5448b4fd91afc7274fa6feae9d32a7aea946a007
enriquecamargo <2024030456@upsin.edu.mx>
2026-04-01 01:01:30 -0700

    feat: Mapeode entidad Notificacion

--- src/main/java/mx/edu/upsite/demo/Entities/Notificacion.java
+++ src/main/java/mx/edu/upsite/demo/Entities/Notificacion.java
@@ -1,4 +1,54 @@
 package mx.edu.upsite.demo.Entities;
 
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Notificacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_notificacion")
+    private Integer id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_emisor")
+    private Usuario emisor;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ref_publicacion")
+    private Publicacion publicacion;
+
+   
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ref_comentario")
+    private Comentario comentario;
+
+    @Column(name = "leido")
+    private Boolean leido = false;
+
+    @Column(name = "enviado_email")
+    private Boolean enviadoEmail = false;
+
+    @CreationTimestamp
+    @Column(name = "fecha", updatable = false)
+    private OffsetDateTime fecha;
+
 }

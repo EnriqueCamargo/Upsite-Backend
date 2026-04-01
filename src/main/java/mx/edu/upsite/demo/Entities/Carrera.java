66a11865940fd3823642d685c931ab4b22e3696e
enriquecamargo <2024030456@upsin.edu.mx>
2026-04-01 01:05:41 -0700

    feat: Mapeode entidad Carrera

--- src/main/java/mx/edu/upsite/demo/Entities/Carrera.java
+++ src/main/java/mx/edu/upsite/demo/Entities/Carrera.java
@@ -1,4 +1,24 @@
 package mx.edu.upsite.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "carreras")
 public class Carrera {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_carrera")
+    private Integer id;
+
+    @Column(name = "nombre", nullable = false, unique = true, length = 100)
+    private String nombre;
 }

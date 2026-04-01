80bc2827476b6ac26d26cc69653b049266424656
enriquecamargo <2024030456@upsin.edu.mx>
2026-04-01 01:20:09 -0700

    feat: Mapeode entidad LikePublicacion

--- src/main/java/mx/edu/upsite/demo/Entities/LikePublicacion.java
+++ src/main/java/mx/edu/upsite/demo/Entities/LikePublicacion.java
@@ -1,4 +1,33 @@
 package mx.edu.upsite.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+import mx.edu.upsite.demo.Entities.id.LikePublicacionID;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.OffsetDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "likes_publicaciones")
 public class LikePublicacion {
+
+    @EmbeddedId
+    private LikePublicacionID id = new LikePublicacionID();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("idPublicacion") // Se vincula con el campo del LikePublicacionId
+    @JoinColumn(name = "id_publicacion")
+    private Publicacion publicacion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("idUsuario") // Se vincula con el campo del LikePublicacionId
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @CreationTimestamp
+    @Column(name = "fecha_like", updatable = false)
+    private OffsetDateTime fechaLike;
 }
--- src/main/java/mx/edu/upsite/demo/Entities/id/LikePublicacionID.java
+++ src/main/java/mx/edu/upsite/demo/Entities/id/LikePublicacionID.java
@@ -1,4 +1,17 @@
 package mx.edu.upsite.demo.Entities.id;
 
-public class LikePublicacionID {
+import jakarta.persistence.Embeddable;
+import lombok.*;
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class LikePublicacionID implements Serializable {
+
+    private Integer idPublicacion;
+    private Integer idUsuario;
 }

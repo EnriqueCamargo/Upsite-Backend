cbf48c99fbb4d7d9a9c8e9d08304e559adb6f18e
rigelb126 <rigelb126@gmail.com>
2026-04-03 13:57:40 -0700

    AuthController listo

--- src/main/java/mx/edu/upsite/demo/Controllers/AuthController.java
+++ src/main/java/mx/edu/upsite/demo/Controllers/AuthController.java
@@ -1,4 +1,29 @@
 package mx.edu.upsite.demo.Controllers;
 
+import lombok.RequiredArgsConstructor;
+import mx.edu.upsite.demo.Services.GoogleAuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final GoogleAuthService googleAuthService;
+
+    @PostMapping("/google")
+    public ResponseEntity<String> loginWithGoogle(@RequestBody Map<String, String> body) {
+        try {
+            String token = googleAuthService.loginWithGoogle(body.get("token"));
+            return ResponseEntity.ok(token);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token inválido");
+        }
+    }
 }
